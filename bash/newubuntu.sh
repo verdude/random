9cfe0d3994a46886bcd0a9595c030986306ea619
Santiago <santiago.verdu.01@gmail.com>
2015-10-04 23:31:48 -0600

    new ubuntu install script

---
+++ bash/newubuntu.sh
@@ -0,0 +1,15 @@
+# sublime text 3
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
+
+# chrome
+sudo apt-get -y install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+
+# git
+sudo apt-get install git
+
+# vim
+sudo apt-get install vim
