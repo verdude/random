8d903b51aace4819ac7c00eeb17f1d0f5669f9b5
Santiago <santiago.verdu.01@gmail.com>
2015-12-30 22:00:01 -0600

    Mostly Finished New Ubuntu Script.

--- bash/lamp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get -y install apache2
-# ifconfig eth0 | grep inet | awk '{ print $2 }'
-sudo apt-get -y install mysql-server
-sudo mysql_secure_installation
-sudo apt-get install php5 php-pear php5-mysql
-sudo service apache2 restart
