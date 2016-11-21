aa70715b7de34d01ab804986b52a0f858359419a
Santiago Verdu <santiago.verdu.01@gmail.com>
2016-11-21 06:10:45 -0700

    can send mail. still funky though

--- python/email/send_email.py
+++ python/email/send_email.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python
 
-import gmail
 import json
 import argparse
 import logging
 import os
+
+def send(args, creds):
+    """
+        Expected credentials:
+            content=the text content of the email, string
+            subject=the email subject, string
+            from=the outgoing email, string
+            password=the outgoing email password, string
+            to=the target emails, list of strings
+    """
+    import smtplib
+    from email.mime.text import MIMEText
+
+    msg = MIMEText(args.message)
+
+    msg['Subject'] = args.subject
+    msg['From'] = str(creds["name"])
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.login(str(creds["email"]), str(creds["password"]))
+    s.sendmail(msg['From'], args.to, msg.as_string())
+    s.quit()
 
 def get_creds(config_filename=""):
     if config_filename is "":
@@ -18,7 +41,9 @@ def get_creds(config_filename=""):
 
 def parse_options():
     parser = argparse.ArgumentParser(prog="updates", description="Send and Email.", add_help=True)
-    parser.add_argument("-m", "--message", action="store", help="The email body")
+    parser.add_argument("-m", "--message", action="store", help="The email body", required=True)
+    parser.add_argument("-t", "--to", action="store", required=True)
+    parser.add_argument("-s", "--subject", action="store")
     parser.add_argument("-d", "--debug", action="store_true", help="set logging to debug")
     parser.add_argument("-q", "--quiet", action="store_true", help="set logging to quiet")
     return parser.parse_args()
@@ -39,6 +64,7 @@ def main():
     if creds is None:
         return
     logging.debug(creds)
+    send(args, creds)
 
 main()
 
