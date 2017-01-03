c83fc7413b8c37c93c34bbda5286f62de904c58a
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-01-03 12:25:49 -0700

    activator fix

--- bash/activator.sh
+++ bash/activator.sh
@@ -6,6 +6,7 @@ else
     cd ~
     wget https://downloads.typesafe.com/typesafe-activator/1.3.12/typesafe-activator-1.3.12.zip
     unzip typesafe-activator-1.3.12.zip
-    mv typesafe-activator-1.3.12.zip activator
+    mv "typesafe-activator-1.3.12" activator
+    ~/activator/bin/activator $@
 fi
 
