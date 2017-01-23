594aee6cd8ad1a5766ece9d449c3a242e41cba36
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-01-23 13:39:02 -0700

    setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
-# download docs
-# get dot files
-# get emacs files
-# add scripts
-# setup git
+get_docs () {
+    scp snt@spooq.website:~/docs.tgz.enc ~
+    openssl enc -d -aes-256-cbc docs.tgz.enc
+}
 
+get_dotfiles () {
+    if [[ -d ~/docs/dotfiles ]]; then
+        echo "copying dotfiles"
+        cp ~/docs/dotfiles/.* ~
+        rm -rf ~/docs/dotfiles
+    else
+        echo "~/docs/dotfiles not found"
+    fi
+    if [[ -d ~/docs/.emacs.d ]]; then
+        echo "copying emacs conf"
+        cp -r ~/docs/.emacs.d ~
+        rm -rf ~/docs/.emacs.d
+    else
+        echo "~/docs/.emacs.d not found"
+    fi
+}
+
+setup_git () {
+    got_git=$(which git)
+    if [[ -z $got_git ]]; then
+        sudo apt install git
+    fi
+    mkdir ~/github && cd github
+    git clone https://github.com/verude/random && cd random
+    ./add_scripts.sh
+    git_setup
+}
 
