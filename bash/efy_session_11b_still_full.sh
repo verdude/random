f644152e19d06fdde887f964d51a2f12b1503bf3
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-01-24 15:35:33 -0700

    efy checker 2017

---
+++ bash/efy_session_11b_still_full.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ ! -d ~/docs/11b ]]; then
+    mkdir -p ~/docs/11b
+fi
+filename=~/docs/11b/$(date +%m%d_%H-%M.html)
+curl -sSL https://efy.byu.edu/efy_session/10087614 | grep -v "<" > $filename
+
+isFull=$(grep '<p class="center"><a href="#" disabled="disabled" title="Full" class="button btn btn-closed">Full</a></p>' $filename)
+if [[ -z $isFull ]]; then
+    ~/bin/send_text -m "11b EFY is not full anymore. $filename"
+    ~/bin/send_email -m"https://efy.byu.edu/efy_session/10087614 $filename" -s"11b open" -t"santiago.verdu.01@gmail.com"
+else
+    echo "STILL FULL" >> $filename
+fi
+
