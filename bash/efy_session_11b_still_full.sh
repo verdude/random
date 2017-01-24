97bdf9d8444421d2753df5249feaa6e54fdfb9cf
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-01-24 15:45:17 -0700

    fix

--- bash/efy_session_11b_still_full.sh
+++ bash/efy_session_11b_still_full.sh
@@ -4,7 +4,7 @@ if [[ ! -d ~/docs/11b ]]; then
     mkdir -p ~/docs/11b
 fi
 filename=~/docs/11b/$(date +%m%d_%H-%M.html)
-curl -sSL https://efy.byu.edu/efy_session/10087614 | grep -v "<" > $filename
+curl -sSL https://efy.byu.edu/efy_session/10087614 | grep "<" > $filename
 
 isFull=$(grep '<p class="center"><a href="#" disabled="disabled" title="Full" class="button btn btn-closed">Full</a></p>' $filename)
 if [[ -z $isFull ]]; then
