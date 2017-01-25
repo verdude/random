55b2d4e8c67cb6395a4dfa16d57b7e57fab43439
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-01-25 15:00:34 -0700

    s

--- bash/backup.sh
+++ bash/backup.sh
@@ -7,6 +7,42 @@ if [[ ! -d docs ]]; then
 fi
 dryrun=$([[ "$1" = "--dry-run" ]] && echo true)
 
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
+
+commit_ () {
+    git commit -m "backup"
+    git push $(git remote) $(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+}
+
+repos_commit () {
+    for f in $(find ~/docs -name ".repos"); do
+        pushd $(dirname $f)
+        for d in $(cat .repos | awk '{print $4}'); do
+            if [[ -d $d ]]; then
+                pushd $d
+                git update-index -q --refresh 
+                untracked=$(git ls-files --others)
+                changed=$(git diff-files | awk '{print $6}')
+                if [[ -n $untracked ]]; then
+                    git add .
+                elif [[ -n $changed ]]; then
+                    for filepath in $changed; do
+                        git add $filpath
+                    done
+                fi
+                popd
+            fi
+        done
+        popd
+    done
+}
+
 dotfiles(){
     cd ~
     if [[ ! -d docs/dotfiles ]]; then
@@ -41,6 +77,7 @@ compress_encrypt() {
     fi
     echo "compressing docs..."
     excludes=$(gitxclude)
+    repos_commit
     [[ -n $dryrun ]] && echo $excludes && cleanup && exit 0
     tar $excludes -cjf docs.bz2 docs
     openssl enc -aes-256-cbc -in docs.bz2 -out docs.bz2.enc
@@ -58,7 +95,7 @@ backup () {
 
 cleanup () {
     echo "cleanup..."
-    rm docs.bz2
+    [[ -f docs.bz2 ]] && rm docs.bz2
     rm -rf ~/docs/dotfiles
     rm -rf ~/docs/.emacs.d
     echo "Done"
