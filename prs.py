65edadd7feeb41010470631a0fbfac6e6a5d93b4
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-02-05 00:34:27 -0700

    remove random file

--- prs.py
+++
@@ -1,5 +0,0 @@
-import sys;
-input = sys.stdin.read().split("\n");
-files = [(int(x.split("\t")[0]), x.split("\t")[1:][0]) for x in input if len(x)>0];
-for tup in files:
-    print"%i, \"%s\""%(tup[0],tup[1])
