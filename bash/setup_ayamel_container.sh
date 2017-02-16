874aa1221e4811e731b4b11398e77b45f63a2c93
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-02-16 16:39:09 -0700

    docker setup

---
+++ bash/setup_ayamel_container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ~
+mkdir -p Documents/Docker/ && cd Documents/Docker
+git clone https://github.com/dro248/ayamelDBDockerfile
+git clone https://github.com/dro248/AyamelDockerfile
+git clone https://github.com/dro248/runAyamel
+
+scp arclite@sartre4.byu.edu:~/application.conf ./AyamelDockerfile
+
+sudo service mysql stop
+
+cd runAyamel
+sudo docker-compose up
+
+
