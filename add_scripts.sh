c0cffa7dd7ebc4452409270ba313270ef04841ef
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-04-15 19:42:42 -0600

    remove folder

--- add_scripts.sh
+++ add_scripts.sh
@@ -2,9 +2,8 @@
 
 cd $GITDIR/random
 rel=$(echo ~/bin/)
-ln -s python/twilio/send_text.py "$rel"
-ln -s python/email/send_email.py "$rel"
-ln -s bash/activator.sh "$rel"
-ln -s bash/wiface.sh "$rel"
-ln -s bash/delswaps.sh "$rel"
+
+for file in $(ls thechosenones); do
+    ln -s $(readlink -e "thechosenones/file") "$rel"
+done
 
