efc83215d424543bddf0608f5a17611cf82a1b2d
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-04-15 23:08:51 -0600

    setup stuff

--- setup.sh
+++ setup.sh
@@ -5,11 +5,7 @@ set -e
 rmd=$([[ "$1" = "-rmd" ]] && echo true)
 
 setup_git () {
-    got_git=$(which git)
-    if [[ -z $got_git ]]; then
-        sudo apt install git
-    fi
-    chmod 775 bash/git_setup.sh
+    sudo apt install git
     git_setup
 }
 
@@ -23,15 +19,16 @@ setup_tmux() {
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
 
-del_folders () {
+setup_folders () {
     for x in $(ls ~ | grep "^[A-Z]"); do
         rm -rf ~/$x
     done
+    mkdir ~/git
 }
 
-del_folders
 ./add_scripts.sh
 setup_git
+setup_folders
 setup_vim
 setup_tmux
 
