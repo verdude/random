86a68dced1766a95ea05ddc92031b6ff6d6c53c0
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-11-10 14:50:36 -0800

    update docker script for ubuntu version 17

--- langs/bash/docker_compose_install.sh
+++ langs/bash/docker_compose_install.sh
@@ -2,22 +2,35 @@
 
 sudo apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl software-properties-common
 
-curl -silent -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+if [[ -z $(lsb_release -r | grep "17") ]]; then
 
-has=$(apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D)
+    curl -silent -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+    has=$(apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D)
 
-if [[ -z $has ]]; then
-    echo "Error adding key. exiting"
-    exit 1
+    if [[ -z $has ]]; then
+        echo "Error adding key. exiting"
+        exit 1
+    fi
+
+    sudo add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
+
+    sudo apt update >/dev/null 2>&1
+    sudo apt -y install docker-engine
+
+else
+    curl -silent -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    has=$(sudo apt-key fingerprint 0EBFCD88)
+
+    if [[ -z $has ]]; then
+        echo "Error adding key. exiting"
+        exit 1
+    fi
+
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu zesty stable"
+
+    sudo apt-get update
+    sudo apt-get install docker-ce
 fi
-
-sudo add-apt-repository \
-    "deb https://apt.dockerproject.org/repo/ \
-    ubuntu-$(lsb_release -cs) \
-    main"
-
-sudo apt update >/dev/null 2>&1
-sudo apt -y install docker-engine
 
 # DOCKER COMPoSE
 sudo curl -L "https://github.com/docker/compose/releases/download/1.13.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
