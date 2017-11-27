347f4e454b3ae835d0f0a9456d3ea17de08bc9f4
Santiago Verdu <santiago.verdu.01@gmail.com>
2017-11-27 01:35:13 -0800

    update env scripts

--- thechosenones/create_start_virtualenv.sh
+++ thechosenones/create_start_virtualenv.sh
@@ -3,27 +3,29 @@
 environment=""
 
 for file in $(ls); do
-   if [ -d "$file" ]; then
+   if [[ -d "$file" ]]; then
        cd "$file"
-       if [ -f "pip-selfcheck.json" ] && [ -d bin ]; then
-            cd ..
+       if [[ -f "pip-selfcheck.json" ]] && [[ -d bin ]]; then
             environment="$file"
+            cd ..
        else
            cd ..
        fi
    fi
 done
 
-if [ -n "$environment" ]; then
+if [[ -z "$environment" ]]; then
+    echo "creating virtual environment"
     virtualenv env
+    environment="env"
 fi
 
 . "$environment/bin/activate"
 
 # make script check if there is a requirements file and 
 # and then load it in
-req=`ls -1 req*.txt`
-if [ ! -z $req ]; then
+req=$(ls -1 req*.txt)
+if [[ -n $req ]]; then
     pip install -r $req
 fi
 
