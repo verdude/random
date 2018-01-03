84a5c3b7b49362eadfff655fab2eb41226c6d9e4
Santiago Verdu <santiago.verdu.01@gmail.com>
2018-01-03 14:01:21 -0600

    minor annotate changes

--- thechosenones/annotate.py
+++ thechosenones/annotate.py
@@ -30,28 +30,33 @@ class Annotator():
         for repo in os.listdir(self.gitdir):
             os.chdir(a(os.environ.get("GITDIR"),repo))
             print("-----------------------------")
-            print(os.getcwd())
             try:
                 remote_name = subprocess.check_output(["git", "remote"], stderr=n).strip("\n").strip(" ")
                 if remote_name == "":
                     continue
                 remote_url = subprocess.check_output(["git", "remote", "get-url", "--all", remote_name], stderr=n).strip("\n").strip(" ")
                 self.commands.append("git clone %s %s" % (remote_url, repo))
+                print(repo)
             except Exception, e:
                 print("broke on %s" % repo)
                 print(str(e))
         n.close()
 
     def update_file(self):
+        print("=============================")
         if self.ex:
             with open(a(self.home,self.f), "r") as repo_file:
                 l = repo_file.readlines()
         else:
             l = []
         for x in l:
+            print("-----------------------------")
+            x = x.strip(" ").strip("\n")
+            if x == "":
+                continue
             repo = x.split(" ")[2]
             if repo not in map(lambda s: s.split(" ")[2], self.commands):
-                print("appending %s" % x)
+                print("[%s] not currently cloned" % x)
                 self.commands.append(x)
         with open(a(self.home,self.f), "w") as repo_file:
             repo_file.write("\n".join(self.commands)+"\n")
