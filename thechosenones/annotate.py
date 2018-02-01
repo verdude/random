88de9cf8851ea27ff1213202048bea703438a59e
Santiago Verdu <santiago.verdu.01@gmail.com>
2018-01-31 17:59:09 -0800

    annotate

--- thechosenones/annotate.py
+++ thechosenones/annotate.py
@@ -7,6 +7,11 @@ import sys
 def a(s1, s2):
     return s1.strip() + "/" + s2.strip()
 
+# parse out the name of the repository
+# from the remote string
+def r(s):
+    return s.split("/")[-1].lower()
+
 class Annotator():
     def __init__(self, repos=".repos.txt"):
         if not os.environ.has_key("GITDIR"):
@@ -29,14 +34,12 @@ class Annotator():
         n = open(os.devnull,"w")
         for repo in os.listdir(self.gitdir):
             os.chdir(a(os.environ.get("GITDIR"),repo))
-            print("-----------------------------")
             try:
                 remote_name = subprocess.check_output(["git", "remote"], stderr=n).strip("\n").strip(" ")
                 if remote_name == "":
                     continue
                 remote_url = subprocess.check_output(["git", "remote", "get-url", "--all", remote_name], stderr=n).strip("\n").strip(" ")
                 self.commands.append("git clone %s %s" % (remote_url, repo))
-                print(repo)
             except Exception, e:
                 print("broke on %s" % repo)
                 print(str(e))
@@ -50,19 +53,25 @@ class Annotator():
         else:
             l = []
         for x in l:
-            print("-----------------------------")
             x = x.strip(" ").strip("\n")
             if x == "":
                 continue
-            repo = x.split(" ")[2]
-            if repo not in map(lambda s: s.split(" ")[2], self.commands):
+            remote = r(x.split(" ")[2])
+            if len(remote) == 0:
+                print("Incorrectly formatted repository remote url: %s" % x.split(" ")[2])
+                continue
+            if remote not in map(lambda s: r(s.split(" ")[2]), self.commands):
                 print("[%s] not currently cloned" % x)
+                print("-----------------------------")
                 self.commands.append(x)
         with open(a(self.home,self.f), "w") as repo_file:
             repo_file.write("\n".join(self.commands)+"\n")
 
     def annotate(self):
         self.ex = self.get_file()
+        if not self.ex:
+            print("[Error]: Could not find %s" % self.f)
+            return
         self.get_remotes()
         self.update_file()
 
