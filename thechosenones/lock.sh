c646dfa2ba2d30e1ff0a54e10ef911773653de31
Santiago Verdu <santiago.verdu.01@gmail.com>
2018-09-18 14:15:59 -0600

    commitall commit

--- thechosenones/lock.sh
+++ thechosenones/lock.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 4% -scale 2500% /tmp/screen.png
+
+set -e
+
+[[ -n "$(which scrot)" ]] && scrot /tmp/screen.png && convert /tmp/screen.png -scale 4% -scale 2500% /tmp/screen.png
 [[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-i3lock -i /tmp/screen.png
-rm /tmp/screen.png
+if [[ -f /tmp/screen.png ]]; then
+    img="-i /tmp/screen.png"
+else
+    img="-c 000000"
+fi
+i3lock $img
