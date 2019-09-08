99352a7da437408e5c474469d85cdbc485bc1122
Santiago Verdu <santiago.verdu.01@gmail.com>
2019-09-07 21:39:37 -0600

    waitlist

---
+++ langs/bash/waitlist.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+LOGFILE=${1:-~/.waitlist.log}
+
+log () {
+    echo $1 >> $LOGFILE
+}
+
+IFS=
+html=$(curl -s 'https://replace')
+if [ $? -ne 0 ]; then
+    log $html
+    log "Failed to get response."
+    send_text -m "failed to get response"
+    exit 1
+fi
+
+class=$(echo $html | grep -B 11 "Arts, Michael A T M")
+if [ $? -ne 0 ]; then
+    log $html
+    log "Class not found."
+    send_text -m "Class was not found"
+    exit 1
+fi
+
+line=$(echo $class | head -1)
+spots=$(echo $line | tr -dc '0-9')
+
+if [ $spots -ne 0 ]; then
+    log "$spots spot(s) available."
+    send_text -m "$spots are available"
+fi
