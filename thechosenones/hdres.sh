4154a7e1766e7534d6db36f7bea21a1d0223efb6
Santiago Verdu <erra@fastmail.com>
2021-03-09 20:17:08 -0700

    sodfij

--- thechosenones/hdres.sh
+++ thechosenones/hdres.sh
@@ -1,3 +1,3 @@
-xrandr --newmode "2560x1440" 241.50 2560 2600 2632 2720 1440 1443 1448 1481 -hsync +vsync
-xrandr --addmode Virtual-0 2560x1440
-xrandr --output Virtual-0 --mode 2560x1440
+xrandr --newmode "1920x1080" 241.50 2560 2600 2632 2720 1440 1443 1448 1481 -hsync +vsync
+xrandr --addmode Virtual-1 1920x1080
+xrandr --output Virtual-1 --mode 1920x1080
