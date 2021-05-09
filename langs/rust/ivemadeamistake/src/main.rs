a9bf119f021db7e4561edf51b0384628358fe641
Santiago Verdu <santiagoverdu01@gmail.com>
2021-05-08 20:06:25 -0600

    working i think

--- langs/rust/ivemadeamistake/src/main.rs
+++ langs/rust/ivemadeamistake/src/main.rs
@@ -3,6 +3,7 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::{thread, time};
+use std::path::Path;
 
 fn get_string(f: &mut File) -> String {
     let mut string = String::new();
@@ -17,7 +18,7 @@ fn sleep(n: u64) {
 
 fn print_time(time: u64) {
     let sixty: f64 = 60.0;
-    println!("In Seconds: {}", time);
+    println!("\nIn Seconds: {}", time);
     println!("In Hours: {}", time as f64 / sixty / sixty);
 }
 
@@ -32,9 +33,10 @@ fn get_fname() -> Result<String, String> {
 }
 
 fn main() {
-    let fname: String = get_fname().expect("No Filename");
+    let home = env::var("HOME").unwrap() + "/.mistakes/";
+    let homeclone = home.clone();
+    let fname: String = home + &get_fname().expect("No Filename");
     let fclone = fname.clone();
-    println!("{}", fname);
     let time = match File::open(fname) {
         Ok(mut r) => get_string(&mut r).parse::<u64>().unwrap(),
         Err(_) => {
@@ -49,8 +51,19 @@ fn main() {
     ctrlc::set_handler(move || {
         let n = time + now.elapsed().as_secs();
         print_time(n);
-        std::fs::write(fclone.as_str(), n.to_string()).expect(format!("failure to write number: {}", n.to_string()).as_str());
-        std::process::exit(0);
+        if ! Path::new(&homeclone).exists() {
+            match std::fs::create_dir_all(&homeclone) {
+                Err(e) => println!("{}", e),
+                _ => println!("created mistakes dir")
+            }
+        }
+        std::process::exit(match std::fs::write(fclone.as_str(), n.to_string()) {
+            Ok(_) => 0,
+            Err(_) => {
+                println!("failure to write to file: {}", fclone);
+                1
+            }
+        });
     }).expect("Error setting Ctrl-C handler");
 
     loop {
