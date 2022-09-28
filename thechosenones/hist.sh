69b14ffdf15a81a7b18261f9cde4692f21aa92be
Santiago Verdu <santi@getlovage.com>
2022-09-28 17:43:45 -0600

    cool mode

--- thechosenones/hist.sh
+++ thechosenones/hist.sh
@@ -1,17 +1,44 @@
 #!/usr/bin/env bash
 
+set -e
+
 cc=$(git rev-parse HEAD)
 args=$@
+index=0
+size=0
+tempfile=""
+hashes=()
+
+function get_hashes() {
+  if [[ -n "$args" ]]; then
+    hashes=($(git log --pretty=format:"%h" -- $args))
+    size=${#hashes[@]}
+  fi
+}
 
 function next() {
+  if [[ $index -lt $size ]]; then
+    cc=${hashes[0]}
+  else
     cc=$(git rev-parse $cc^)
+  fi
+
+  if [[ $index -lt $size ]]; then
+    index=$(($index + 1))
+    cc=${hashes[$index]}
+  elif [[ $size -ne 0 ]]; then
+    echo "finished."
+    exit
+  fi
 }
 
 function log() {
-    git diff $cc^ $cc -- $args
-    echo $cc $(git show --pretty=format:"%B" --no-patch $cc)
-    next
+  git diff $cc^ $cc -- $args
+  git show --oneline --pretty=format:"%h %f %al" --no-patch $cc
+  next
 }
+
+get_hashes
 
 while
   log
