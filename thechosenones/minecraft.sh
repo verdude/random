8d494e8f9696a6ccd53201d9675a91f7e3b43178
Santiago Verdu <santi@getlovage.com>
2022-10-16 21:49:19 -0600

    versions

--- thechosenones/minecraft.sh
+++ thechosenones/minecraft.sh
@@ -3,7 +3,7 @@
 set -eu
 
 sudo apt update
-sudo apt install openjdk-16-jdk-headless
+sudo apt install openjdk-18-jdk-headless
 curl -O https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar
-java -Xmx1024M -Xms1024M -jar minecraft_server.1.19.2.jar nogui
+java -Xmx1024M -Xms1024M -jar server.jar nogui
 
