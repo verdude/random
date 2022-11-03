6abb32dce7d66d01384fa55c3894d8812ffcc782
Santiago Verdu <santi@getlovage.com>
2022-11-02 20:31:14 -0600

    config arg

--- thechosenones/checker.sh
+++ thechosenones/checker.sh
@@ -5,13 +5,15 @@ set -euo pipefail
 cn=""
 msg=""
 cache=""
+configarg=""
 
-while getopts :hm:c: flag
+while getopts :hf:m:c: flag
 do
   case "${flag}" in
     c) cn="${OPTARG}";;
     m) msg="${OPTARG}";;
     h) cache="yeah";;
+    f) configarg="-c ${OPTARG}";;
     :) echo "Argument required for option: -${OPTARG}"; exit 1;;
     ?) echo "Bad argument: -${OPTARG}"; exit 1;;
   esac
@@ -32,7 +34,7 @@ out="$(tcli | grep "${cn}")"
 
 if [[ -n "$out" ]]; then
   echo 1 > $CACHE
-  send_text -m "${msg:-$0}"
+  send_text -m "${msg:-$0}" ${configarg}
 elif [[ -f $CACHE ]]; then
   rm $CACHE
 fi
