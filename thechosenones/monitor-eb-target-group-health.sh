ec6e9f12ec8390c7c55aec14c63ded6c91f437da
Santiago Verdu <santi@getlovage.com>
2022-11-03 18:32:48 -0600

    monitor

---
+++ thechosenones/monitor-eb-target-group-health.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -ueo pipefail
+
+profile=""
+environment=""
+
+function usage() {
+  echo "Usage:"
+  echo "  Required Arguments:"
+  echo "    -e <environment name> # required. Elastic Beanstalk env."
+  echo
+  echo "  Optional Arguments:"
+  echo "    -p <aws profile name> # optional"
+  echo "    -x                    # optional. verbose mode."
+  echo "    -h"
+  echo
+  echo "jq is required."
+  exit ${1:-0}
+}
+
+while getopts :e:hp:x flag
+do
+  case ${flag} in
+    x) set -x;;
+    p) profile="--profile ${OPTARG}";;
+    e) environment="${OPTARG}";;
+    h) usage;;
+    :) echo "arg required for: -${OPTARG}"; usage 1;;
+    ?) echo "invalid arg: -${OPTARG}"; usage 1;;
+  esac
+done
+
+if [[ -z "$environment" ]]; then
+  usage 1
+fi
+
+lbarn=$(aws $profile elasticbeanstalk describe-environment-resources --environment-name $environment | jq .EnvironmentResources.LoadBalancers[0].Name | tr -d '"')
+targetgrouparn=$(aws $profile elbv2 describe-target-groups --load-balancer-arn $lbarn | jq .TargetGroups[0].TargetGroupArn | tr -d '""')
+
+truncate -s 0 target-deploy.log
+
+while true; do
+  sclear="true"
+  for x in {1..10}; do
+    state=$(aws $profile elbv2 describe-target-health --target-group-arn $targetgrouparn | jq .TargetHealthDescriptions[0].TargetHealth.State)
+
+    if [[ -n "${sclear}" ]]; then
+      clear
+      sclear=""
+    fi
+
+    printf "$(date) - ${state}\n" | tee -a target-deploy.log > /dev/null
+    echo ${state}
+  done
+done
