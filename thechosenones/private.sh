2eaa3ef41619b55aa120b3ce20ab3f4671f5649f
Santiago Verdu <santi@getlovage.com>
2022-11-05 01:47:05 -0600

    priv

---
+++ thechosenones/private.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DOTDIR=${DOTDIR:-}
+name="p"
+efile="p.enc"
+dfile="$name.tar.bz2"
+pfile="$HOME/.secretpw"
+decrypt=""
+update=""
+cipher="chacha20"
+keyderivation="pbkdf2"
+rmflags="-f"
+untar=""
+tarcomp="z"
+private=(
+  .secrets.sh
+  .texterrc
+  .bin/
+)
+
+while getopts cdp:oxyt flag
+do
+  case ${flag} in
+    c) update="yeah";;
+    d) decrypt="-d";;
+    p) pfile="${OPTARG}";;
+    y) rmflags="-i";;
+    t) untar="yeah";;
+    x) set -x;;
+    :) echo "-${OPTARG}: Requires an argument."; exit 1;;
+    ?) echo "-${OPTARG}: Invalid argument."; exit 1;;
+  esac
+done
+
+# remove temp files
+function _cleanup() {
+  rm $rmflags *.tar.bz2
+}
+trap _cleanup EXIT
+
+# decrypt or encrypt private files depending on
+# whether -d was passed in or not.
+function enc() {
+  if [[ ! -f "$pfile" ]]; then
+    echo "enc: $pfile not found."
+    exit 1
+  fi
+
+  local dec=${1:-}
+  local infile="$efile"
+  local outfile="$dfile"
+
+  if [[ $dec != "-d" ]]; then
+    dec=""
+    infile="$dfile"
+    outfile="$efile"
+    tar --mtime=0 -c${tarcomp}f "$dfile" "${private[@]}"
+  elif [[ ! -f "$efile" ]]; then
+    echo "enc: $efile not found. cannot decrypt."
+    exit 1
+  fi
+
+  openssl enc $dec -pass "file:$pfile" -$cipher \
+    -in "$infile" -out "$outfile" -$keyderivation
+
+  if [[ -n "$untar" ]]; then
+    tar x${tarcomp}f "$dfile"
+  fi
+}
+
+# check if private files have been changed.
+# writes to stdout if there was a change.
+function check() {
+  enc -d
+  local oldhash=$(sha1sum "$dfile" | cut -d' ' -f1)
+  local newhash=$oldhash
+  local newf="$(openssl rand -hex 5).temp.tar.bz2"
+
+  tar --mtime=0 -c${tarcomp}f "$newf" "${private[@]}"
+  newhash=$(sha1sum "$newf" | cut -d' ' -f1)
+
+  if [[ "$oldhash" != "$newhash" ]]; then
+    echo changed
+  fi
+}
+
+if [[ -z "$DOTDIR" ]] || [[ ! -d "$DOTDIR" ]]; then
+  echo Weird.
+  exit 1
+fi
+
+cd "${DOTDIR}"
+
+if [[ -n "$update" ]]; then
+  changed=$(check)
+  if [[ -n "$changed" ]]; then
+    enc
+    echo "Updated."
+  else
+    echo "No change."
+  fi
+else
+  enc $decrypt
+fi
