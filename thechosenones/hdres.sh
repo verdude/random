4a5c6a6923b6ff3cd0a4d00f78a325ef39892de1
Santiago Verdu <erra@qmake.org>
2023-03-19 23:05:23 -0600

    i

--- thechosenones/hdres.sh
+++ thechosenones/hdres.sh
@@ -1,13 +1,7 @@
-set -x
+set -xEeuo pipefail
 
-if [[ "$1" = "1440" ]]; then
-  xrandr --output Virtual-1 --mode 2560x1440
-elif [[ "$1" = "1496" ]]; then
-  xrandr --newmode "1496x1000"  123.70  1496 1584 1744 1992  1000 1001 1004 1035  -HSync +Vsync
-  xrandr --addmode Virtual-1 1496x1000
-  xrandr --output Virtual-1 --mode 1496x1000
-else
-  xrandr --newmode "1920x1080" 241.50 2560 2600 2632 2720 1440 1443 1448 1481 -hsync +vsync
-  xrandr --addmode Virtual-1 1920x1080
-  xrandr --output Virtual-1 --mode 1920x1080
-fi
+xrandr --addmode HDMI2 2560x1440
+xrandr --output HDMI2 --mode 2560x1440 --rate 144
+xrandr --output DP1 --mode 2560x1440 --rate 144
+xrandr --output HDMI2 --left-of DP1
+xrandr --output eDP1 --off
