ee3400b77c710c3fe140590b6de027cc11f71797
Santiago Verdu <erra@qmake.org>
2023-03-20 10:07:42 -0600

    Primary monitor

--- thechosenones/hdres.sh
+++ thechosenones/hdres.sh
@@ -1,7 +1,7 @@
 set -xEeuo pipefail
 
 xrandr --addmode HDMI2 2560x1440
-xrandr --output HDMI2 --mode 2560x1440 --rate 144
+xrandr --output HDMI2 --primary --mode 2560x1440 --rate 144
 xrandr --output DP1 --mode 2560x1440 --rate 144
 xrandr --output HDMI2 --left-of DP1
 xrandr --output eDP1 --off
