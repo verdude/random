d359f1b3d7f67ea8d4e1a8328835618bca4d9a71
Santiago Verdu <santiagoverdu01@gmail.com>
2023-07-24 00:28:10 -0600

    ok for now hehe haha

--- thechosenones/hdres.sh
+++ thechosenones/hdres.sh
@@ -1,7 +1,14 @@
 set -xEeuo pipefail
 
-xrandr --addmode HDMI2 2560x1440
-xrandr --output HDMI2 --primary --mode 2560x1440 --rate 144
-xrandr --output DP1 --mode 2560x1440 --rate 144
-xrandr --output HDMI2 --left-of DP1
-xrandr --output eDP1 --off
+mapfile -t screens < <(xrandr | grep " connected" | awk '{print $1}')
+
+if [[ "${#screens[@]}" -eq 1 ]] && [[ "${screens[0]}" == "Virtual-1" ]]; then
+  xrandr --addmode Virtual-1 2560x1440
+  xrandr --output Virtual-1 --primary --mode 2560x1440
+else
+  xrandr --addmode HDMI2 2560x1440
+  xrandr --output HDMI2 --primary --mode 2560x1440 --rate 144
+  xrandr --output DP1 --mode 2560x1440 --rate 144
+  xrandr --output HDMI2 --left-of DP1
+  xrandr --output eDP1 --off
+fi
