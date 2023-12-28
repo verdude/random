d3b4c05639ddf80886a4f04ea23a8b66606c9cb5
Santiago Verdu <erra@qmake.org>
2023-12-28 13:14:18 -0700

    apt pyenv

---
+++ thechosenones/apt-pyenv-install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo apt install zlib1g \
+  zlib1g-dev \
+  libssl-dev \
+  libbz2-dev \
+  libsqlite3-dev \
+  libedit-dev \
+  liblzma-dev \
+  tk-dev \
+  libreadline-dev \
+  libffi-dev \
+  curl
+
+curl https://pyenv.run | bash -
+
+spyenv
+
+pyenv install 3.6 3.11
