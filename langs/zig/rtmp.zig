58eb4d2048f387fb5ed9e1d93c4adc64c5b01314
Santiago Verdu <erra@qmake.org>
2023-12-31 15:33:24 -0700

    allocPrint

--- langs/zig/rtmp.zig
+++ langs/zig/rtmp.zig
@@ -7,19 +7,6 @@ const rename = std.os.rename;
 
 const BuildError = error{NotEnoughMemory};
 
-fn buildstr(pre: []const u8, suf: []const u8, mem: []u8) BuildError![]u8 {
-    if (pre.len + suf.len > mem.len) {
-        return BuildError.NotEnoughMemory;
-    }
-    var start_bound: usize = 0;
-    var end_bound: usize = start_bound + pre.len;
-    @memcpy(mem[start_bound..end_bound], pre);
-    start_bound = end_bound;
-    end_bound = start_bound + suf.len;
-    @memcpy(mem[start_bound..end_bound], suf);
-    return mem[0..end_bound];
-}
-
 pub fn main() !u8 {
     const len: u16 = 4096;
     var buf: [len]u8 = undefined;
@@ -48,12 +35,11 @@ pub fn main() !u8 {
         return 1;
     };
 
-    var new_name: []u8 = try alloc.alloc(u8, tmpdir_prefix.len + name.len);
+    var new_name: []u8 = try std.fmt.allocPrint(alloc, "{s}{s}", .{ tmpdir_prefix, name });
     defer alloc.free(new_name);
-    const name_slice: []u8 = try buildstr(tmpdir_prefix, name, new_name);
 
-    std.log.info("{s} -> {s}", .{ base, name_slice });
+    std.log.info("{s} -> {s}", .{ base, new_name });
     try std.os.chdir(dn);
-    try rename(base, name_slice);
+    try rename(base, new_name);
     return 0;
 }
