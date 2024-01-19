6ca41d80b3da651cda32a760b4de77b04e7bcdc6
santi <erra@qmake.org>
2024-01-18 23:13:48 -0700

    switch up fonts

--- langs/zig/fl/src/main.zig
+++ langs/zig/fl/src/main.zig
@@ -54,8 +54,7 @@ pub fn main() !u8 {
 
     const ft = [_]FontType{
         FontType{ .type = "xft", .name = "Hermit" },
-        FontType{ .type = "xft", .name = "DejaVuSans" },
-        FontType{ .type = "xft", .name = "Linux Libertine" },
+        FontType{ .type = "xft", .name = "DejaVuSansMono" },
         FontType{ .type = "xft", .name = "Noto Sans CJK SC" },
         FontType{ .type = "xft", .name = "Noto Color Emoji" },
     };
