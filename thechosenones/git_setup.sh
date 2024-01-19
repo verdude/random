2e73d121d01f7674115c8f1470f7b4849c149c1e
santi <erra@qmake.org>
2024-01-19 15:37:55 -0700

    libsecret credential helper

--- thechosenones/git_setup.sh
+++ thechosenones/git_setup.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
+set -eEuo pipefail
+
 got_git=$(which git)
 if [ -z $got_git ]; then
     echo "git is not installed"
     exit 1
 fi
 
-generate() {
-    # generates ssh keys
-    echo "Make sure to name the key pair 'ed25519'"
+function generate() {
     ssh-keygen -t ed25519 -C $EMAIL
     ssh_agent=`eval "$(ssh-agent -s)"`
     if [[ ! -z ssh_agent ]]; then
-        ssh-add ~/.ssh/id_rsa
-        echo "Adding id_rsa to ssh-agent"
+        ssh-add ~/.ssh/id_ed25519
+        echo "Adding id_ed25519 to ssh-agent"
     else
         echo "ssh_agent not found"
     fi
     got_xclip=`which xclip`
     if [[ -z $got_xclip ]]; then
         echo "Downloading xclip"
-        sudo apt-get install xclip
+        sudo apt-get install xclip || sudo pacman -S xclip
     fi
-    cat ~/.ssh/id_rsa.pub | xclip -sel clip
+    cat ~/.ssh/id_ed25519.pub | xclip -sel clip
     echo "Now add the key to github. (It's in the paste buffer)"
 }
 
-confirm() {
+function confirm() {
     read -r -p "$1" response
     case $response in
         [yY][eE][sS]|[yY])
@@ -41,16 +41,32 @@ confirm() {
     esac
 }
 
-echo $NAME $EMAIL
-if [[ -z "$NAME" ]] || [[ -z "$EMAIL" ]]; then
-    echo 'Need $NAME and $EMAIL'
-    exit 1
-fi
-git config --global user.name "$NAME"
-git config --global user.email "$EMAIL"
-git config --global alias.slog "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-git config --global push.default simple
+function gitconfig() {
+  if [[ -z "$NAME" ]] || [[ -z "$EMAIL" ]]; then
+    git config --global user.name "$NAME"
+    git config --global user.email "$EMAIL"
+  else
+    echo 'Need $EMAIL/$NAME to generate keys'
+    echo "skipping name/email config"
+  fi
+
+  git config --global alias.slog "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+  git config --global push.default simple
+
+  gclpath=/usr/lib/git-core/git-credential-libsecret
+  if [[ -f $gclpath ]]; then
+    git config --global credential.helper "$gclpath"
+  else
+    echo "skipping libsecret credential helper config"
+  fi
+}
+
+gitconfig
 
 if confirm "Generate keys? [y/N]"; then
+    if [[ -z "$EMAIL" ]]; then
+        echo 'Need $EMAIL to generate keys'
+        exit 1
+    fi
     generate
 fi
