59fe01653abd1e9c448da7be55fd48509d1f3642
Santiago Verdu <erra@qmake.org>
2025-11-07 14:03:48 -0700

    sientate y piensa en algo

--- langs/rust/binary_search.rs
+++ langs/rust/binary_search.rs
@@ -19,6 +19,18 @@ fn binarySearch(nums: &[i32], target: i32) -> i32 {
     return -1;
 }
 
+fn findFirstOccurrence(nums: &[i32], target: i32) -> i32 {
+    let mut i = binarySearch(nums, target);
+    if i == -1 {
+        return -1;
+    }
+
+    while i > 0 && nums[i as usize - 1] == target {
+        i -= 1;
+    }
+    return i;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,5 +47,20 @@ mod tests {
         assert_eq!(binarySearch(&nums[0..1], 42), -1);
         assert_eq!(binarySearch(&nums[0..1], 1), 0);
         assert_eq!(binarySearch(&nums[0..2], 3), 1);
+    }
+
+    #[test]
+    fn test_find_first() {
+        let nums = [1, 3, 5, 5, 5, 5, 5, 5, 5, 7, 9];
+        assert_eq!(findFirstOccurrence(&nums, 1), 0);
+        assert_eq!(findFirstOccurrence(&nums, 5), 2);
+        assert_eq!(findFirstOccurrence(&nums[0..3], 5), 2);
+        assert_eq!(findFirstOccurrence(&nums, 9), 10);
+        assert_eq!(findFirstOccurrence(&nums, 2), -1);
+        assert_eq!(findFirstOccurrence(&nums, 10), -1);
+        assert_eq!(findFirstOccurrence(&nums[0..0], 42), -1);
+        assert_eq!(findFirstOccurrence(&nums[0..1], 42), -1);
+        assert_eq!(findFirstOccurrence(&nums[0..1], 1), 0);
+        assert_eq!(findFirstOccurrence(&nums[0..2], 3), 1);
     }
 }
